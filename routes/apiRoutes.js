2f272cddbb39372eee764fd5a46fde225efe49da
cshepscorp <sheppard.christy@gmail.com>
2021-11-10 18:53:08 -0500

    notes will delete

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,11 +18,11 @@ router.get('/notes', (req, res) => {
         savedNotesString = JSON.stringify(savedNotes);
         console.log(savedNotesString + 'note was parsed in GET method');
     });
-  });
+});
   
-  /* POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-  */
-  router.post('/notes', (req, res) => { // used to be app.post
+/* POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
+*/
+router.post('/notes', (req, res) => { // used to be app.post
     // With POST requests, we can package up data, typically as an object, and send it to the server
     // req.body property is where we can access that data on the server side and do something with it
       let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
@@ -34,8 +34,25 @@ router.get('/notes', (req, res) => {
       fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
       console.log("Note saved to db.json. Content: ", newNote);
       res.json(savedNotes);
-  });
-  
+});
+// delete a note
+// const deleteNote = (id) =>
+//   fetch(`/api/notes/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
+/* DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.*/
+router.delete('/notes/:id', (req, res) => {
+  // read all notes from the db.json file
+  let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
+  //let noteToDelete = req.body.id;
+  let newSavedNotes = savedNotes.filter(note => note.id !== req.params.id );
+  fs.writeFileSync("./db/db.json", JSON.stringify(newSavedNotes));
+  res.json(newSavedNotes);
+  console.log('removed note with the id of: ' + req.params.id)
+});
 
 
 module.exports = router;
