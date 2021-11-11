3435780081a3bd8012323d9301572e5acd5ea224
cshepscorp <sheppard.christy@gmail.com>
2021-11-10 19:48:40 -0500

    unique ids working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,58 +1,50 @@
 const router = require('express').Router();
 const fs = require('fs');
+// generate unique id for notes
+const { v4 : uuidv4 } = require('uuid');
 
-//const { getAndRenderNotes, getNotes } = require('../public/assets/js/index');
-// const path = require('path');
-
-// GET /api/notes should read the db.json file and return all saved notes as JSON.
+// GET /api/notes should read the db.json file and .
 // route that the front-end can request data from
-/* the get() method requires two arguments. 
-1. a string that describes the route the client will have to fetch from. 
-2. a callback function that will execute every time that route is accessed with a GET request. */
-
 router.get('/notes', (req, res) => {
-
+    // read the db.json file
     fs.readFile("./db/db.json", (err, data) => {
         let savedNotes = JSON.parse(data);
+        // return all saved notes as JSON
         res.json(savedNotes);
+        // a string that describes the route the client will have to fetch from. 
         savedNotesString = JSON.stringify(savedNotes);
-        console.log(savedNotesString + 'note was parsed in GET method');
+        // console.log(savedNotesString + 'note was parsed in GET method');
     });
 });
-  
-/* POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-*/
-router.post('/notes', (req, res) => { // used to be app.post
-    // With POST requests, we can package up data, typically as an object, and send it to the server
-    // req.body property is where we can access that data on the server side and do something with it
+
+// With POST requests, we can package up data, typically as an object, and send it to the server
+router.post('/notes', (req, res) => {
+    
+      // read all notes from the db.json file
       let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-      let newNote = req.body;
-      let uniqueID = (savedNotes.length).toString();
-      newNote.id = uniqueID;
-      savedNotes.push(newNote);
-  
+      // req.body property is where we can access that data on the server side and do something with it
+      let newNote = req.body; // create new note by pulling in new data via req.body
+      let uniqueID = uuidv4(); // automatically generate new unique id
+      newNote.id = uniqueID; // add that unique id to the record of the note
+      savedNotes.push(newNote); // add new note to list of notes
+      
       fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-      console.log("Note saved to db.json. Content: ", newNote);
+      // console.log("Note saved to db.json. Content: ", newNote); // is this working?
+      // return the new note to the client
       res.json(savedNotes);
 });
-// delete a note
-// const deleteNote = (id) =>
-//   fetch(`/api/notes/${id}`, {
-//     method: 'DELETE',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//   });
+
 /* DELETE /api/notes/:id should receive a query parameter containing the id of a note to delete. In order to delete a note, you'll need to read all notes from the db.json file, remove the note with the given id property, and then rewrite the notes to the db.json file.*/
 router.delete('/notes/:id', (req, res) => {
   // read all notes from the db.json file
   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-  //let noteToDelete = req.body.id;
+  // save new array that's used savedNotes and filtered out the note with the unique id we want to delete
   let newSavedNotes = savedNotes.filter(note => note.id !== req.params.id );
+  // re-write the json file to update db
   fs.writeFileSync("./db/db.json", JSON.stringify(newSavedNotes));
+  // send results back to browser
   res.json(newSavedNotes);
-  console.log('removed note with the id of: ' + req.params.id)
+  // console.log('removed note with the id of: ' + req.params.id); // is this working?
 });
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,15 +1,10 @@
 // the function of this file is to start the server 💡
 
-//const fs = require('fs');
-//const path = require('path'); // path provides utilities for working with file and directory paths
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express(); // instantiate the server
 const fs = require('fs');
-// generates unique user ID
-// const ShortUniqueId = require('short-unique-id');
-// const uid = new ShortUniqueId();
-// const { notes } = require('./db/db.json');
+
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
@@ -19,32 +14,6 @@ const htmlRoutes = require('./routes/htmlRoutes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-
-// app.get('/api/notes', (req, res) => {
-//   //console.log(notes);
-//   fs.readFile("./db/db.json", (err, data) => {
-//       let savedNotes = JSON.parse(data);
-//       res.json(savedNotes);
-//       savedNotesString = JSON.stringify(savedNotes);
-//       console.log(savedNotesString + 'note was parsed in get method');
-//   });
-// });
-
-/* POST /api/notes should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. You'll need to find a way to give each note a unique id when it's saved (look into npm packages that could do this for you).
-*/
-// app.post('/api/notes', (req, res) => { // used to be app.post
-//   // With POST requests, we can package up data, typically as an object, and send it to the server
-//   // req.body property is where we can access that data on the server side and do something with it
-//   let savedNotes = JSON.parse(fs.readFileSync("./db/db.json", "utf8"));
-//     let newNote = req.body;
-//     let uniqueID = (savedNotes.length).toString();
-//     newNote.id = uniqueID;
-//     savedNotes.push(newNote);
-
-//     fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
-//     console.log("Note saved to db.json. Content: ", newNote);
-//     res.json(savedNotes);
-// });
 
 // Any time a client navigates to <ourhost>/api, the app will use the router we set up in apiRoutes. If / is the endpoint, then the router will serve back our HTML routes.
 app.use('/api', apiRoutes);
